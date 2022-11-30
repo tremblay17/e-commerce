4fe922a7ccb4e8609b9ae4ca1fb7c5c92001fa84
Reid-Sew02 <72675552+Reid-Sew02@users.noreply.github.com>
2022-11-30 09:37:51 -0600

    Add files via upload
    fixed queries

--- cart.py
+++ cart.py
@@ -10,21 +10,21 @@ class CART:
     
     def get_CartContents(self,username):
         #returns all items (and their quantities) in given user's cart
-        res = self.db.exeQuery(f'SELECT itemID, quantity_in_cart FROM CART WHERE uname = "{username}";')
+        res = self.db.exeQuery(f'SELECT itemID, quantity_in_cart FROM CARTS WHERE uname = "{username}";')
         return res
         
     def Remove_Item(self,username, itemID):
         #deletes selected item from given user's cart
-        res = self.db.exeQuery(f'DELETE FROM CART WHERE itemID = "{itemID}" AND uname = "{username}";') 
+        res = self.db.exeQuery(f'DELETE FROM CARTS WHERE itemID = "{itemID}" AND uname = "{username}";') 
     
     def get_TotalCost(self,username):
         #returns the combined cost of all items in CART belonging to the user
-        res = self.db.exeQuery(f'SELECT SUM(item_price * quantity_in_cart) FROM CART WHERE uname = "{username}";')
+        res = self.db.exeQuery(f'SELECT SUM(item_price * quantity_in_cart) FROM CARTS WHERE uname = "{username}";')
         return res
 
     def Checkout(self,username):
         #decreases inventory stock by amount of given items in user's cart  
-        res1 = self.db.exeQuery(f'UPDATE INVENTORY JOIN CART ON INVENTORY.itemID = CART.itemID SET INVENTORY.quantity = INVENTORY.quantity - CART.quantity_in_cart WHERE CART.uname = "{username}";')
+        res1 = self.db.exeQuery(f'UPDATE INVENTORY JOIN CARTS ON INVENTORY.itemID = CARTS.itemID SET INVENTORY.quantity = INVENTORY.quantity - CARTS.quantity_in_cart WHERE CARTS.uname = "{username}";')
 
         cart = CART()
         totalCost = cart.get_TotalCost(username)
@@ -35,4 +35,4 @@ class CART:
         #adds an order
         res3 = self.db.exeQuery(f'INSERT INTO ORDERS (uname, numItems, subtotal) VALUES ("{username}","{numItems}","{totalCost}");')
         #deletes all of given user's items from CART
-        res2 = self.db.exeQuery(f'DELETE FROM CART WHERE uname = "{username}";')
+        res2 = self.db.exeQuery(f'DELETE FROM CARTS WHERE uname = "{username}";')
